c7fa734e8a6cbc7d616e1eab81179e401382eae0
Andy Situ <55203867+gvmby@users.noreply.github.com>
2022-08-02 13:17:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+
+streamlit.title("Breakfast Menu")
+
+streamlit.title("Omega 3 and Oatmeal")
+
+streamlit.title("Kale Spinach Smoothie")
+
+streamlit.title("Hard boiled egg")
