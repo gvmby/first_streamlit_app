1f99060a900fbc5caec2680cfd14dcc0ae01a159
Andy Situ <55203867+gvmby@users.noreply.github.com>
2022-08-02 13:18:47 -0700

    Update streamlit_app.py
    Added .header and .text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit("Breakfast Menu")
+streamlit.header("Breakfast Menu")
 
-streamlit("Omega 3 and Oatmeal")
+streamlit.text("Omega 3 and Oatmeal")
 
-streamlit("Kale Spinach Smoothie")
+streamlit.text("Kale Spinach Smoothie")
 
-streamlit("Hard boiled egg")
+streamlit.text("Hard boiled egg")
